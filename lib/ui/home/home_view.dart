af4e183da94ee75f2e51bb3f45a75ef3295bc434
fizhu <hvyz.anbiya@gmail.com>
2022-04-17 15:41:11 +0700

    adding handling prevent small screen opening site

--- lib/ui/home/home_view.dart
+++ lib/ui/home/home_view.dart
@@ -5,6 +5,7 @@ import 'package:portofolio/ui/home/project/project_view.dart';
 import 'package:portofolio/ui/home/welcome/welcome_view.dart';
 import 'package:portofolio/utils/app_color.dart';
 import 'package:portofolio/utils/ext.dart';
+import 'package:portofolio/widget/text/sub_header_text.dart';
 import 'package:stacked/stacked.dart';
 
 import '../../di/locator.dart';
@@ -34,87 +35,102 @@ class HomeView extends ViewModelBuilderWidget<HomeViewModel> {
     height = Ext.matchParentHeight(context);
     return Scaffold(
         body: SafeArea(
-      child: Row(
-        mainAxisSize: MainAxisSize.max,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          // Container(height: Ext.matchParentHeight(context),
-          //   width: 86,color: AppColor.softBackground,
-          // padding: const EdgeInsets.only(top: Dimen.margin_24, bottom: Dimen.margin_24),
-          // child: Column(
-          //   mainAxisAlignment: MainAxisAlignment.start,
-          //   children: [
-          //     ElevatedButton(
-          //       onPressed: () {
-          //         scrollController.animateTo((0*height), duration: const Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
-          //       },
-          //       child: const Icon(
-          //         Icons.home_outlined,
-          //         color: Colors.white,
-          //       ),
-          //       style: ElevatedButton.styleFrom(
-          //           primary: AppColor.primary,
-          //           onPrimary: Colors.grey[400],
-          //           minimumSize: Size.zero,
-          //           padding: const EdgeInsets.all(Dimen.margin),
-          //           elevation: 0,
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(Dimen.margin),
-          //           )),
-          //     ),
-          //     const SizedBox(height: Dimen.margin,),
-          //     ElevatedButton(
-          //       onPressed: () {
-          //         scrollController.animateTo((1*height), duration: const Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
-          //       },
-          //       child: const Icon(
-          //         Icons.person_outline,
-          //         color: Colors.white,
-          //       ),
-          //       style: ElevatedButton.styleFrom(
-          //           primary: AppColor.primary,
-          //           onPrimary: Colors.grey[400],
-          //           minimumSize: Size.zero,
-          //           padding: const EdgeInsets.all(Dimen.margin),
-          //           elevation: 0,
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(Dimen.margin),
-          //           )),
-          //     ),
-          //     const SizedBox(height: Dimen.margin,),
-          //     ElevatedButton(
-          //       onPressed: () {
-          //         scrollController.animateTo(((1*height) + 1000), duration: const Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
-          //       },
-          //       child: const Icon(
-          //         Icons.all_inbox_outlined,
-          //         color: Colors.white,
-          //       ),
-          //       style: ElevatedButton.styleFrom(
-          //           primary: AppColor.primary,
-          //           onPrimary: Colors.grey[400],
-          //           minimumSize: Size.zero,
-          //           padding: const EdgeInsets.all(Dimen.margin),
-          //           elevation: 0,
-          //           shape: RoundedRectangleBorder(
-          //             borderRadius: BorderRadius.circular(Dimen.margin),
-          //           )),
-          //     ),
-          //   ],
-          // ),),
-          Expanded(child: ListView(
-            physics: const BouncingScrollPhysics(),
-            controller: scrollController,
-            children: const [
-              WelcomeView(),
-              AboutView(),
-              ProjectView(),
-              SizedBox(height: Dimen.margin_128,)
-            ],
-          ),)
-
-        ],
-      )
-    ));
+            child: Row(
+      mainAxisSize: MainAxisSize.max,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        // Container(height: Ext.matchParentHeight(context),
+        //   width: 86,color: AppColor.softBackground,
+        // padding: const EdgeInsets.only(top: Dimen.margin_24, bottom: Dimen.margin_24),
+        // child: Column(
+        //   mainAxisAlignment: MainAxisAlignment.start,
+        //   children: [
+        //     ElevatedButton(
+        //       onPressed: () {
+        //         scrollController.animateTo((0*height), duration: const Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
+        //       },
+        //       child: const Icon(
+        //         Icons.home_outlined,
+        //         color: Colors.white,
+        //       ),
+        //       style: ElevatedButton.styleFrom(
+        //           primary: AppColor.primary,
+        //           onPrimary: Colors.grey[400],
+        //           minimumSize: Size.zero,
+        //           padding: const EdgeInsets.all(Dimen.margin),
+        //           elevation: 0,
+        //           shape: RoundedRectangleBorder(
+        //             borderRadius: BorderRadius.circular(Dimen.margin),
+        //           )),
+        //     ),
+        //     const SizedBox(height: Dimen.margin,),
+        //     ElevatedButton(
+        //       onPressed: () {
+        //         scrollController.animateTo((1*height), duration: const Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
+        //       },
+        //       child: const Icon(
+        //         Icons.person_outline,
+        //         color: Colors.white,
+        //       ),
+        //       style: ElevatedButton.styleFrom(
+        //           primary: AppColor.primary,
+        //           onPrimary: Colors.grey[400],
+        //           minimumSize: Size.zero,
+        //           padding: const EdgeInsets.all(Dimen.margin),
+        //           elevation: 0,
+        //           shape: RoundedRectangleBorder(
+        //             borderRadius: BorderRadius.circular(Dimen.margin),
+        //           )),
+        //     ),
+        //     const SizedBox(height: Dimen.margin,),
+        //     ElevatedButton(
+        //       onPressed: () {
+        //         scrollController.animateTo(((1*height) + 1000), duration: const Duration(milliseconds: 500), curve: Curves.fastOutSlowIn);
+        //       },
+        //       child: const Icon(
+        //         Icons.all_inbox_outlined,
+        //         color: Colors.white,
+        //       ),
+        //       style: ElevatedButton.styleFrom(
+        //           primary: AppColor.primary,
+        //           onPrimary: Colors.grey[400],
+        //           minimumSize: Size.zero,
+        //           padding: const EdgeInsets.all(Dimen.margin),
+        //           elevation: 0,
+        //           shape: RoundedRectangleBorder(
+        //             borderRadius: BorderRadius.circular(Dimen.margin),
+        //           )),
+        //     ),
+        //   ],
+        // ),),
+        if (Ext.matchParentWidth(context) > 1000)
+          Expanded(
+            child: ListView(
+              physics: const BouncingScrollPhysics(),
+              controller: scrollController,
+              children: const [
+                WelcomeView(),
+                AboutView(),
+                ProjectView(),
+                SizedBox(
+                  height: Dimen.margin_128,
+                )
+              ],
+            ),
+          ),
+        if (Ext.matchParentWidth(context) < 1000)
+          const Expanded(
+            child: Center(
+              child: Padding(
+                padding: EdgeInsets.all(Dimen.margin),
+                child: SubHeaderText(
+                  text:
+                      'Opps! sorry, This site is currently not supported for this screen resolution.',
+                ),
+              ),
+            ),
+          )
+      ],
+    )));
   }
 }
